503acca3d31f3a235d61132a0659ef0c3d754b04
夜法之书(appotry) <andycrusoe@gmail.com>
2025-10-10 02:16:51 +0800

    Update embyLaunchPotplayer.js
    版本更新

--- embyWebAddExternalUrl/embyLaunchPotplayer.js
+++ embyWebAddExternalUrl/embyLaunchPotplayer.js
@@ -4,7 +4,7 @@
 // @name:zh      embyLaunchPotplayer
 // @name:zh-CN   embyLaunchPotplayer
 // @namespace    http://tampermonkey.net/
-// @version      1.1.21
+// @version      1.2.1
 // @description  emby/jellfin launch extetnal player
 // @description:zh-cn emby/jellfin 调用外部播放器
 // @description:en  emby/jellfin to external player
